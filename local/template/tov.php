c2d4fddd045d31a2f1635fdc0580ffaef5358450
Дарья <d.popkova@maximaster.ru>
2016-04-06 19:31:37 +0300

    Работа с товарами.

--- local/template/tov.php
+++ local/template/tov.php
@@ -1,7 +1,49 @@
 <?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
 CModule::IncludeModule("iblock");
-
-//добавления элемента
 $el = new CIBlockElement;
-include 'export_file_599506_2.txt';
+$handle = fopen("tovari.txt", "r");
+while (!feof($handle)) {
+    $line = fgets($handle);
+
+    $pieces = explode(",", $line);
+    print_r($pieces);
+    $save=CFile::SaveFile("img/1.jpg", "/iblock/acf/");
+
+
+    $arFields = Array(
+        "IBLOCK_SECTION_ID" => $pieces[6],
+        "IBLOCK_ID" => 4,
+
+        "DETAIL_PICTURE" => CFile::MakeFileArray($_SERVER["DOCUMENT_ROOT"]."/img/1.jpg"),
+
+
+        "NAME" => $pieces[0],
+
+        "IN_SECTIONS" =>  "$pieces[2]",
+        "ARTNUMBER" => $pieces[3],
+        "GROUP0" => $pieces[4],
+
+
+
+
+
+    );
+    $id = $el->Add($arFields);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+fclose($handle);
+
+
+
