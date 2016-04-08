1b37d69613ad42621c4adda87fcbf8107e52d9d1
Дарья <d.popkova@maximaster.ru>
2016-04-08 13:26:21 +0300

    Работа с справочниками.

--- local/template/tov.php
+++ local/template/tov.php
@@ -1,7 +1,52 @@
 <?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
 
-CModule::IncludeModule("iblock");
+//CModule::IncludeModule("iblock");
+//CModule::IncludeModule('highloadblock'); //модуль highload инфоблоков
+array_map('CModule::IncludeModule', ['iblock', 'highloadblock']);
+use Bitrix\Highloadblock as HL;
+use Bitrix\Main\Entity;
+
+$hlblock_requests=HL\HighloadBlockTable::getById(3)->fetch();//requests
+$entity_requests=HL\HighloadBlockTable::compileEntity($hlblock_requests);
+$entity_requests_data_class = $entity_requests->getDataClass();
+$main_query_requests = new Entity\Query($entity_requests_data_class);
+$main_query_requests->setSelect(array('ID','UF_NAME'));
+$main_query_requests->setFilter(
+    array(
+        'UF_NAME'=>'Радуга',
+    )
+);
+$result_requests = $main_query_requests->exec();
+$result_requests = new CDBResult($result_requests);
+
+while ($row_requests=$result_requests->Fetch()) {
+    $requests[] = $row_requests; //массив выбранных элементов
+}
+//print_r($requests[0][UF_NAME]);
+
+/*
+$result = $entity_requests_data_class::add(array( //добавляем элемент
+    'UF_NAME'=>'Rhfcbds',
+    'UF_DESCRIPTION'=>'Бла-бла',
+));
+*/
+
+
+
+
+/*if ( !($arData = $hlblock->fetch()) ){
+    echo 'Инфоблок не найден';
+}
+else{
+    echo 'Инфоблок найден';
+}*/
+
+
+
+
+
+
 //сначала выбрать информацию о ней из базы данных
 /*$hldata = Bitrix\Highloadblock\HighloadBlockTable::getById($ID)->fetch();
 
@@ -44,8 +89,9 @@ echo $_SERVER["DOCUMENT_ROOT"]."/local/template/img/".$pieces[5]."";
     }
 
 
+
     $arFields = Array(
-        "IBLOCK_SECTION_ID" => $pieces[6],
+        "IBLOCK_SECTION_ID" => $pieces[7],
         "IBLOCK_ID" => 4,
 
 
@@ -59,9 +105,10 @@ echo $_SERVER["DOCUMENT_ROOT"]."/local/template/img/".$pieces[5]."";
         "PROPERTY_VALUES"=> [
             "ARTNUMBER" => $pieces[4],
             "DESCRIPTION" => $pieces[3],
-
+            "BRAND_REF" => 	$pieces[6],
 
         ],
+
 
 
 
