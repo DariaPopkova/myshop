26703b4d91e5b55706723fed4443bfcb99271036
Дарья <d.popkova@maximaster.ru>
2016-04-27 12:08:23 +0300

    Работа  с карточкой_1.

--- local/components/my/cart/templates/.default/template.php
+++ local/components/my/cart/templates/.default/template.php
@@ -1,26 +1,33 @@
+<? foreach ($arResult as $category): ?>
 
-
-    <? foreach($arResult as $category): ?>
-        <?print_r($category['CHARACTERISTICS']);?>
-        <h2><?echo $category['NAME'];?></h2>
-            <div id="cartfon">
-
-                <div id="imgcart"> <img src="<?=$category['DETAIL_PICTURE'];?>"  ></div>
-                <div id="char">
-                    <div id="naz_cart">
-                        <div class="prop_naz">Цвет:</div>
-                        <div class="prop_naz">Артикул:</div>
-                        <div class="prop_naz">Производитель:</div>
-                        <div class="prop_naz">Бренд:</div>
-                    </div>
-                    <div class="prop_tov"><?echo $category['DESCRIPTION'];?></div>
-                    <div class="prop_tov"><?echo $category['ARTNUMBER'];?></div>
-                    <div class="prop_tov"><?echo $category['MANUFACTURER'];?></div>
-                    <div class="prop_tov"><?echo $category['BRAND'];?></div>
+    <h2><? echo $category['NAME']; ?></h2>
+    <div id="cartfon">
+        <div id="pokupka">
+            <div id="price_cart">
+                <div class="prop_naz_cart"><h3>Цена:</h3></div>
+                <div class="prop_cart">
+                    <? echo $category['PRICE']; ?>
                 </div>
-                <div id="opisanie">
-                    <div class="prop_tov"><?echo $category['CHARACTERISTICS'];?></div>
-                </div>
-
             </div>
-    <? endforeach; ?>
+        </div>
+        <p style="text-align: center"><button>Купить</button>
+        <div id="imgcart"><img src="<?= $category['DETAIL_PICTURE']; ?>"></div>
+        <div id="opisanie">
+            <div class="prop_tov"><? echo $category['CHARACTERISTICS']; ?></div>
+        </div>
+        <div id="char">
+            <div id="naz_cart">
+                <div class="prop_naz">Цвет:</div>
+                <div class="prop_naz">Артикул:</div>
+                <div class="prop_naz">Производитель:</div>
+                <div class="prop_naz">Бренд:</div>
+            </div>
+            <div class="prop_tov"><? echo $category['DESCRIPTION']; ?></div>
+            <div class="prop_tov"><? echo $category['ARTNUMBER']; ?></div>
+            <div class="prop_tov"><? echo $category['MANUFACTURER']; ?></div>
+            <div class="prop_tov"><? echo $category['BRAND']; ?></div>
+        </div>
+
+
+    </div>
+<? endforeach; ?>
