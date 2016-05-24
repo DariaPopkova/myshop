263e6c378444a276dfe428b90dad8b49fc68ade1
Дарья <d.popkova@maximaster.ru>
2016-05-24 13:09:36 +0300

    Работа над ошибками_theend.

--- local/components/my/catalog/component.php
+++ local/components/my/catalog/component.php
@@ -13,22 +13,6 @@ $brandDataClass = HL\HighloadBlockTable::compileEntity(
     HL\HighloadBlockTable::getById(HLIBLOCK_BRANDS)
         ->fetch()
 )->getDataClass();
-if($sectionID > 0)
-{
-    $search_section = CIBlockSection::GetList(
-        array(),
-        array(
-            'ACTIVE' => 'Y',
-            'IBLOCK_ID' => IBLOCK_PRODUCTS,
-            'ID' => $sectionID
-        )
-    )->GetNext();
-
-    if(empty($search_section))
-    {
-        LocalRedirect("/catalog.php");
-    }
-}
 if($brandID > 0)
 {
     $brand = $brandDataClass::getList(array(
@@ -51,21 +35,64 @@ if($brandID > 0)
         LocalRedirect("/404.php", "404 Not Found");
     }
 }
+if($sectionID > 0)
+{
+    $search_section = CIBlockSection::GetList(
+        array(),
+        array(
+            'ACTIVE' => 'Y',
+            'IBLOCK_ID' => IBLOCK_PRODUCTS,
+            'ID' => $sectionID
+        )
+    )->GetNext();
+
+    if(empty($search_section))
+    {
+        LocalRedirect("/catalog.php");
+    }
+
+    $arResult['SECTIONS'][] = array_merge(
+        array(
+            'MAIN' => 'Y'
+        ),
+        $search_section
+    );
+    $arFilter['SECTION_ID'] = $sectionID;
+    if ( ! empty($brandXML_ID))
+    {
+        $arFilter['PROPERTY_BRAND_REF'] = $brandXML_ID;
+        $arFilter['INCLUDE_SUBSECTIONS'] = "Y";
+    }
+    $searchElement = CIBlockElement::GetList(
+        array(),
+        $arFilter,
+        false,
+        false,
+        [
+            'ID', 'IBLOCK_ID','IBLOCK_SECTION_ID', 'NAME', 'DETAIL_PICTURE', 'SECTION_ID', 'CATALOG_GROUP_1'
+        ]
+    );
+    while($product = $searchElement->GetNextElement())
+    {
+        $arFields = $product->GetFields();
+        $arProps = $product->GetProperties();
+
+        foreach($arProps as $pid => $prop)
+        {
+            $arProps[$pid] = CIBlockFormatProperties::GetDisplayValue($arFields, $prop);
+        }
+
+        $arFields['DETAIL_PICTURE'] = CFile::GetPath($arFields["DETAIL_PICTURE"]);
+        $arFields['PROPERTIES'] = $arProps;
+
+        $arResult['PRODUCTS'][] = $arFields;
+
+    }
+}
+
 /* Разделы каталога */
-$arParentSection = CIBlockSection::GetList(
-    array(),
-    array(
-        'ACTIVE' => 'Y',
-        'IBLOCK_ID' => IBLOCK_PRODUCTS,
-        'ID' => $sectionID
-    )
-)->GetNext();
-$arResult['SECTIONS'][] = array_merge(
-    array(
-        'MAIN' => 'Y'
-    ),
-    $arParentSection
-);
+
+
 //$arResult['NAMESECTION']['NAME'] = $arSection['NAME'];
 $podsection = CIBlockSection::GetList(
     array(),
@@ -84,39 +111,7 @@ while($arPodsection = $podsection->GetNext())
 $arFilter = array(
     'IBLOCK_ID' => IBLOCK_PRODUCTS,
 );
-if ($sectionID > 0)
-{
-    $arFilter['SECTION_ID'] = $sectionID;
-}
-if ( ! empty($brandXML_ID))
-{
-    $arFilter['PROPERTY_BRAND_REF'] = $brandXML_ID;
-    $arFilter['INCLUDE_SUBSECTIONS'] = "Y";
-}
-$searchElement = CIBlockElement::GetList(
-    array(),
-    $arFilter,
-    false,
-    false,
-    [
-        'ID', 'IBLOCK_ID','IBLOCK_SECTION_ID', 'NAME', 'DETAIL_PICTURE', 'SECTION_ID', 'CATALOG_GROUP_1'
-    ]
-);
-while($product = $searchElement->GetNextElement())
-{
-    $arFields = $product->GetFields();
-    $arProps = $product->GetProperties();
 
-    foreach($arProps as $pid => $prop)
-    {
-        $arProps[$pid] = CIBlockFormatProperties::GetDisplayValue($arFields, $prop);
-    }
 
-    $arFields['DETAIL_PICTURE'] = CFile::GetPath($arFields["DETAIL_PICTURE"]);
-    $arFields['PROPERTIES'] = $arProps;
-
-    $arResult['PRODUCTS'][] = $arFields;
-
-}
 
 $this->IncludeComponentTemplate(); // <- $arResult
