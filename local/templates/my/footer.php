3889739772ef712975fe3d624e9a92c536ec22aa
Дарья <d.popkova@maximaster.ru>
2016-06-17 16:02:24 +0300

    Работа с аяксом измененного количества товаров в большой корзине

--- local/templates/my/footer.php
+++ local/templates/my/footer.php
@@ -49,57 +49,13 @@
                         $("#basket_S").html(data);
                     }
                 );
-                var bask_kol = $(".basket_kol").html();
-                alert(bask_kol);
+
             }
 
         });
     });
 
 </script>
-<script>
-   $(function() {
-     //  $( ".basket_item" ).change(function(event) {
-       // $input  = $('.basket_item input');
-       //var input  = document.body.children[0];
-      // var input = document.body.children[0];
-       //input.oninput = function(event) {
-            //event.preventDefault();
 
-            //alert("sucsess");
-          /* var value=$("input#select").val();
-            var max = parseInt($('#select').attr('max'));
-            if (value > max) {
-                alert( 'Это значение больше максимального, т.е больше '+max+'. Введите заново верное значение.' );
-            }
-            else
-            {
-                var query = window.location.search.substring(1);
-                var vars = query.split("&");
-                for (var i=0;i<vars.length;i++) {
-                    var pair = vars[i].split("=");
-                    if(pair[0] == "ID"){
-                        var id = pair[1];
-                    }
-                }
-                $.post(
-                    '/local/templates/my/addbasket.php',
-                    {
-                        kol: value,
-                        ID: id
-                    },
-                    function (data)
-                    {
-                        $("#basket_S").html(data);
-                    }
-                );
-                var bask_kol = $(".basket_kol").html();
-                alert(bask_kol);
-            }*/
-
-       // });
-   // });
-
-</script>
 </body>
 </html>
