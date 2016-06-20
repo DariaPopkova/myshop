0a91dcf99e357e202a8d5b3bf8a973c1a7370ce8
Дарья <d.popkova@maximaster.ru>
2016-06-20 17:43:06 +0300

    Скрипт плюса и минуса

--- local/templates/my/footer.php
+++ local/templates/my/footer.php
@@ -18,75 +18,23 @@
     });
 </script>
 <script>
-    $(function() {
-        $a = $('.cart_aj a');
-        $a.click(function(event) {
-            event.preventDefault();
-            var value=$("input#select").val();
-
-            var max = parseInt($('#select').attr('max'));
-            if (value > max) {
-                alert( 'Это значение больше максимального, т.е больше '+max+'. Введите заново верное значение.' );
-                document.getElementById("select").value = max;
+    $(document).ready(function() {
+        var input = document.getElementById("number_kol");
+        var max = parseInt(document.getElementById("residue_znach").innerHTML);
+        var kol_prev = max - Number(document.getElementById("basket_kol").innerHTML);
+        input.oninput = function()
+        {
+            if(parseInt(input.value) <= kol_prev)
+            {
+                var $input = $(this).parent().find('input');
+                $input.val(count);
+                $input.change();
+                var id_number = kol_prev - parseInt(input.value);
             }
             else
             {
-                var query = window.location.search.substring(1);
-                var vars = query.split("&");
-                for (var i=0;i<vars.length;i++) {
-                    var pair = vars[i].split("=");
-                    if(pair[0] == "ID"){
-                        var id = pair[1];
-                    }
-                }
-                var add = Number($("#basket_kol").text())+Number(value);
-                alert(add);
-                if(add < max)
-                {
-                    $.post(
-                        '/local/templates/my/addbasket.php',
-                        {
-                            kol: value,
-                            ID: id
-                        },
-                        function (data)
-                        {
-                            $("#basket_S").html(data);
-
-                        }
-                    );
-                }
-                else{
-                    alert('Добавляемое значение больше максимально допустимого, т.е больше '+max+'');
-                }
-
-            }
-
-        });
-    });
-
-</script>
-<script>
-
-    $(document).ready(function() {
-
-        var input = document.getElementById("number_kol");
-       var input_prev_val = input.value;
-       // alert(input_prev_val);
-
-        input.oninput = function() {
-            alert(input.value);
-            //alert(input_prev_val);
-           // alert(parseInt(document.getElementById("residue_znach").innerHTML));
-            if(parseInt(input.value) <= parseInt(document.getElementById("residue_znach").innerHTML))
-            {
-                var id_number = parseInt(document.getElementById("residue_znach").innerHTML);
-                id_number -= parseInt(input.value);
-                document.getElementById("residue_znach").innerHTML = id_number;
-                input_prev_val = input.value;
-            }
-            else{
                 alert('Превышено значение ввода!');
+                input.value = kol_prev;
             }
 
         };
@@ -94,34 +42,72 @@
             var $input = $(this).parent().find('input');
             var count = parseInt($input.val()) - 1;
             count = count < 1 ? 1 : count;
-            //if(count<=)
-
-
-            $input.val(count);
-            $input.change();
-            var id_number = parseInt(document.getElementById("residue_znach").innerHTML);
-            id_number += count;
-            document.getElementById("residue_znach").innerHTML = id_number;
-            alert(id_number);
-            /*$.post(
-                'local/components/my/cart/templates/.default/template.php',
-                {
-                    residue: count
-                }
-            );*/
+            if(parseInt($input.val()) > 1)
+            {
+                $input.val(count);
+                $input.change();
+                var id_number = kol_prev - count;
+            }
+            else
+            {
+                alert('Не стоит этого делать.');
+            }
             return false;
         });
         $('.plus').click(function () {
             var $input = $(this).parent().find('input');
             var count = parseInt($input.val()) + 1;
-            $input.val(parseInt($input.val()) + 1);
-            $input.change();
-            var id_number = parseInt(document.getElementById("residue_znach").innerHTML);
-            id_number -= count;
-            document.getElementById("residue_znach").innerHTML = id_number;
+            if(parseInt($input.val()) < kol_prev)
+            {
+                $input.val(parseInt($input.val()) + 1);
+                $input.change();
+                var id_number = kol_prev - count;
+            }
+            else
+            {
+                document.getElementById("number_kol").innerHTML = kol_prev;
+            }
             return false;
         });
+        $a = $('.cart_aj a');
+        document.getElementById("residue_znach").innerHTML = kol_prev;
+        $a.click(function(event)
+        {
+            event.preventDefault();
+            var value = Number(input.value);
+            var query = window.location.search.substring(1);
+            var vars = query.split("&");
+            for (var i=0;i<vars.length;i++)
+            {
+                var pair = vars[i].split("=");
+                if(pair[0] == "ID"){
+                    var id = pair[1];
+                }
+            }
+            if(kol_prev  > 0)
+            {
+                $.post(
+                    '/local/templates/my/addbasket.php',
+                    {
+                        kol: value,
+                        ID: id
+                    },
+                    function (data)
+                    {
+                        $("#basket_S").html(data);
+                        document.getElementById("residue_znach").innerHTML = max - Number(document.getElementById("basket_kol").innerHTML);
+                        input.value = 1;
+                        kol_prev = document.getElementById("residue_znach").innerHTML;
 
+                    }
+                );
+            }
+            else
+            {
+                alert('Товара больше нет!');
+            }
+
+        });
     });
 </script>
 
